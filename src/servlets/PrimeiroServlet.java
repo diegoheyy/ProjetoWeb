ac67b78ca1240961acd71e2be0d27db872dfc0e7
Diego Hey <diegoheyy@gmail.com>
2016-03-15 20:36:39 -0300

    aquivos exemplos
    arquivos usados para exemplos e testes.

---
+++ src/servlets/PrimeiroServlet.java
@@ -0,0 +1,57 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PrimeiroServlet
+ */
+@WebServlet(name = "SegundoServlet", description = "tete", urlPatterns = { "/sv2" })
+public class PrimeiroServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PrimeiroServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter pw = response.getWriter();
+		
+		
+		pw.println("<!DOCTYPE html>");
+		pw.println("<html>");
+		pw.println("<body>");
+		pw.println("<h1> do GET <br/></h1>");
+		
+		int num = 5;
+		for(int i = 0; i<=10; i ++){
+			pw.println("5 x " + i + " = " + (5*i));
+			pw.println("<br/>");
+		}
+		pw.println("</body>");
+		pw.println("</html>");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
